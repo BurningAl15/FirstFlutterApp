604f0b6096e3dd03e1019419d922496ed5deb524
Aldhair <u201315853@upc.edu.pe>
2019-06-02 16:34:47 -0500

    Personalizing app

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,75 @@ import 'package:flutter/material.dart';
 
 final appName="Aldhair Made Flutter App";
 
-final backgroundColor=Colors.lightBlue[200];
-final textDir=TextDirection.rtl;
-final textStyleColor=Colors.grey[200];
+final backgroundColor=Colors.grey[400];
+final textDir=TextDirection.ltr;
+final textStyleColor=[Colors.grey[50],Colors.grey[100],Colors.grey[200],Colors.grey[300]] ;
 final textFontSize=60.0;
-
 final appScreenText="My first app";
+final appBarColor=Colors.grey[600];
+
+List phrases=["1","2","3","4!"];
+
+var index=0;
+
+var phrase=phrases[index];
+
 
 void main(){
   runApp(
-
-    new MaterialApp(
-      title:appName,
-      home: Scaffold(
-        appBar: AppBar(title: Text(appScreenText),),
-        body:
-        Material(
-            color:backgroundColor,
-            child: Center(
-              child: Text(
-                "Aldhair",
-                textDirection: textDir,
-                style: TextStyle(color:textStyleColor,fontSize: textFontSize),
-              ),
-            )
-        )
-      )
-      )
-
+  new MyApp()
 
   );
+}
+
+class MyApp extends StatefulWidget{
+  MyFlutterApp createState() =>new MyFlutterApp();
+}
+
+class MyFlutterApp extends State<MyApp>{
+  void _UpdateStuff(){
+      index++;
+      if(index>phrases.length-1) {
+        index=0;
+        phrase=phrases[index];
+      }
+      phrase=phrases[index];
+
+    print(phrase);
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+        title:appName,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(appScreenText),
+              backgroundColor: appBarColor,
+              actions: <Widget>[
+                IconButton(
+                  icon:Icon(Icons.playlist_play),
+                  tooltip: 'Change value',
+                  onPressed:(){
+                    setState(() {
+                      _UpdateStuff();
+                    });;
+                  }
+                )
+              ],
+            ),
+            body:
+            new   Material(
+                color:backgroundColor,
+                child: Center(
+                  child: Text(
+                    phrase,
+                    textDirection: textDir,
+                    style: TextStyle(color:textStyleColor[index],fontSize: textFontSize),
+                  ),
+                )
+            )
+        )
+    );
+  }
 }
